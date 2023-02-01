ff4980ab2e42cfc335998d3b432fec9123eecbb6
Omar Khaled <omar_hanafy@icloud.com>
2023-02-01 19:19:33 +0200

    update website icon

---
+++ enefty_icons_preview/android/app/src/main/kotlin/com/example/enefty_icons_preview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.enefty_icons_preview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- enefty_icons_preview_website/android/app/src/main/kotlin/com/example/enefty_icons_preview/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.enefty_icons_preview
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity() {
-}
